df810b6dc7683703ff0e8b4cac9de143b032cdcf
gelton cruz <gelton@midiaplural.com.br>
2015-11-12 14:39:41 -0300

    listar

---
+++ listar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Listar produtos</title>
+  </head>
+  <body>
+    <header><h1>Lista de produtos</h1></header>
+    <table border="1">
+      <thead>
+        <th>Codigo</th>
+        <th>Nome</th>
+        <th>Preco</th>
+        <th>Observacao</th>
+      </thead>
+      <tbody>
+    <?php
+     include_once("conexao.php");
+     $query = "SELECT * FROM produto";
+     $result = mysql_query($query);
+     while($linha = mysql_fetch_array($result,MYSQL_ASSOC)){
+    ?>
+        <tr>
+          <td><?=$linha['codigo']; ?></td>
+          <td><?=$linha['nome']; ?></td>
+          <td><?=$linha['preco']; ?></td>
+          <td><?=$linha['descricao'] ?></td>
+          <td><a href="atualizar.php?codigo=<?=$linha['codigo']; ?>">Atualizar</a></td>
+        </tr>
+        <?php
+        }
+        mysql_free_result($result);
+       ?>
+      </tbody>
+    </table>
+
+  </body>
+</html>
